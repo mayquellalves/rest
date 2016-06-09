961e84c758f49a36600b0cd6b998daa5355bb025
Mayquel <mayquellalves@gmail.com>
2016-06-09 19:12:31 -0300

    start project

---
+++ restfull/src/main/java/br/com/k19/models/Banda.java
@@ -0,0 +1,35 @@
+package br.com.k19.models;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Banda {
+	private String nome;
+	private int anoDeFormacao;
+	private int id;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getAnoDeFormacao() {
+		return anoDeFormacao;
+	}
+
+	public void setAnoDeFormacao(int anoDeFormacao) {
+		this.anoDeFormacao = anoDeFormacao;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+}
---
+++ restfull/src/main/java/com/k19/restful/resources/BandaResource.java
@@ -0,0 +1,79 @@
+package com.k19.restful.resources;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import br.com.k19.models.Banda;
+
+@Path("/bandas")
+public class BandaResource {
+
+	// vamos utilizar um Map estático para // "simular" uma base de dados static
+	private static Map<Integer, Banda> bandasMap;
+	static {
+		bandasMap = new HashMap<Integer, Banda>();
+		Banda b1 = new Banda();
+		b1.setId(1);
+		b1.setNome("Led Zeppelin");
+		b1.setAnoDeFormacao(1968);
+		bandasMap.put(b1.getId(), b1);
+
+		Banda b2 = new Banda();
+		b2.setId(2);
+		b2.setNome("AC/DC");
+		b2.setAnoDeFormacao(1973);
+		bandasMap.put(b2.getId(), b2);
+	}
+
+	@GET
+	@Produces("text/xml")
+	public List<Banda> getBandas() {
+		return new ArrayList<Banda>(bandasMap.values());
+	}
+
+	@Path("{id}")
+	@GET
+	@Produces("text/xml")
+	public Banda getBanda(@PathParam("id") int id) {
+		return bandasMap.get(id);
+	}
+
+	@POST
+	@Consumes("text/xml")
+	@Produces("text/plain")
+	public String adicionaBanda(Banda banda) {
+		banda.setId(bandasMap.size() + 1);
+		bandasMap.put(banda.getId(), banda);
+		return banda.getNome() + " adicionado.";
+	}
+
+	@Path("{id}")
+	@PUT
+	@Consumes("text/xml")
+	@Produces("text/plain")
+	public String atualizaBanda(Banda banda, @PathParam("id") int id) {
+		Banda atual = bandasMap.get(id);
+		atual.setNome(banda.getNome());
+		atual.setAnoDeFormacao(banda.getAnoDeFormacao());
+		return banda.getNome() + " atualizada.";
+	}
+
+	@Path("{id}")
+	@DELETE
+	@Produces("text/plain")
+	public String removeBanda(@PathParam("id") int id) {
+		bandasMap.remove(id);
+		return "Banda removida.";
+	}
+}
